e9c2fb5d5c0ce997c0cd782d5d65947739e43dc4
daae16 <ohmokei@gmail.com>
2020-07-29 09:43:36 +0900

    dd

--- src/io/FolderTest2.java
+++ src/io/FolderTest2.java
@@ -4,29 +4,17 @@ import java.io.File;
 
 public class FolderTest2 {
 
-	  public static void main(String[] args) {
-	      
-		   
-	      AddressTest2 at = new AddressTest2();
-	      
-
-	         File path = new File("C:\\java_study\\address\\");
-	         if(path.isDirectory()) {
-
-	            File[] files = path.listFiles();
-	            
-	            for(File file : files) {
-	               
-	               if(!file.isDirectory() && file.getName().indexOf("build_")==0)
-	            
-	                  System.out.println("시작");
-	                    at.exePath(file.toString());
-	                     System.out.println("끝");
-	            }
-	      
-	   }
-	         
-	        System.out.println("전부끝");
-	   }
-	   
+	public static void main(String[] args) {
+		AddressTest2 at = new AddressTest2();
+		File path = new File("C:\\java_study\\address");
+		if(path.isDirectory()) {
+			File[] files = path.listFiles();	
+			for(File file:files) {
+				if(!file.isDirectory() && file.getName().indexOf("build_")==0) {
+					int cnt = at.insertAddress(file);
+					System.out.println(file.getName() + ", 입력갯수 : " + cnt);
+				}
+			}
+		}
 	}
+}
